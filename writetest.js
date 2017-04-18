2cf307e6e559a7262c5182fa1995ed69106106c1
Pavel Šturc <psturc@redhat.com>
2017-04-18 16:19:39 +0200

    Added test of response text

--- writetest.js
+++ writetest.js
@@ -1,44 +1,58 @@
-const fs = require('fs');
-const http = require('http');
+const fs = require('fs')
+const http = require('http')
+const assert = require('assert')
 
-function testFileWriteToPV(pathToFile) {
-  return new Promise(function(res, rej) {
-    fs.writeFile(pathToFile, "test string", function (err) {
-      if (err) {
-        return rej(err);
-      }
-      console.log("Test of write access to the Persistent Volume succeeded");
-      console.log("Data stored in the file in path:", pathToFile);
-      res(pathToFile);
-    });
-  });
+const testString = 'test string'
+const pathToFile = process.env.PATH_TO_TESTFILE
+const port = process.env.PORT
+
+function testFileWriteToPV() {
+  return new Promise(function(resolve) {
+    fs.writeFile(pathToFile, testString, function (err) {
+      assert.ifError(err)
+      console.log('Test of write access to the Persistent Volume succeeded')
+      console.log('Data stored in the path:', pathToFile)
+      resolve()
+    })
+  })
 }
 
-function testFileReadFromPV(pathToFile) {
-  return new Promise(function(res, rej) {
+function testFileReadFromPV() {
+  return new Promise(function(resolve) {
     fs.readFile(pathToFile, 'utf-8', function (err, data) {
-      if (err) {
-        return rej(err);
-      }
-      console.log("Data from PV read successfully. Data:", data);
-      res();
-    });
-  });
+      assert.ifError(err)
+      assert.equal(data, testString, 'String written to file should be equal to the one that has been read')
+      console.log('Data from PV read successfully.')
+      resolve()
+    })
+  })
 }
+
 function startTheServer() {
-  const app = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  });
-  const port = process.env.PORT || 8080;
-  app.listen(port, function() {
-    console.log("HTTP server started successfully on port", port);
-  });
+  return new Promise(function(resolve) {
+    const app = http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'})
+      res.end(testString)
+    })
+    app.listen(port, function() {
+      console.log('HTTP server started successfully on port', port)
+      resolve()
+    })
+  })
 }
-testFileWriteToPV(process.env.PATH_TO_PV)
+
+function readDataFromHttpServer() {
+  http.get('http://localhost:' + 8080, function(res) {
+    res.setEncoding('utf-8')
+    res.on('data', function (data) {
+      assert.equal(data, testString, 'String from file should be equal to response data')
+    })
+  })
+}
+
+
+testFileWriteToPV(pathToFile)
 .then(testFileReadFromPV)
 .then(startTheServer)
-.catch(function (err) {
-  console.error("Error occured:", err);
-})
+.then(readDataFromHttpServer)
 
